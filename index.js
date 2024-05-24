91fdf87fa767f414996b4e12485b543fdaa9521e
Mario Santos Salles <84875639+Marinho-salles@users.noreply.github.com>
2024-05-23 22:43:12 -0300

    subir os arquivos do meu site

---
+++ index.js
@@ -0,0 +1,15 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function(duvida) {
+    duvida.addEventListener('click', function (){
+           duvida.classList.toggle('ativa');
+
+    })
+})
+//function none (argumento) {
+
+//}
+// alert()
+
+
+
